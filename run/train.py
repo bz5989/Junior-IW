933d98f0bb4155f50a8f0da6ec9840b5a40da368
Bryan Zhang <bz5989@princeton.edu>
2024-11-05 17:23:38 -0500

    idk, uncomment some things

--- run/train.py
+++ run/train.py
@@ -503,8 +503,8 @@ def run(ctxt=None):
     # dim_option=2
     policy_q_input_dim = module_obs_dim + args.dim_option
 
-    # if args.algo in ['sac', 'ppo']:
-    #     policy_q_input_dim = module_obs_dim
+    if args.algo in ['sac', 'ppo']:
+        policy_q_input_dim = module_obs_dim
 
     policy_module = module_cls(
         input_dim=policy_q_input_dim,
@@ -573,35 +573,35 @@ def run(ctxt=None):
         max_std=10.0,
     )
     
-    # if args.algo == 'dads':
-    #     skill_dynamics = module_cls(**module_kwargs)
-    # else:
-    #     skill_dynamics = None
-    # if args.algo == 'cic':
-    #     module_cls, module_kwargs = get_gaussian_module_construction(
-    #         args,
-    #         hidden_sizes=master_dims,
-    #         hidden_nonlinearity=nonlinearity or torch.relu,
-    #         w_init=torch.nn.init.xavier_uniform_,
-    #         input_dim=args.dim_option * 2,
-    #         output_dim=args.dim_option,
-    #         layer_normalization=False, #args.phi_layer_norm,
-    #     )
-    #     pred_net = module_cls(**module_kwargs)
+    if args.algo == 'dads':
+        skill_dynamics = module_cls(**module_kwargs)
+    else:
+        skill_dynamics = None
+    if args.algo == 'cic':
+        module_cls, module_kwargs = get_gaussian_module_construction(
+            args,
+            hidden_sizes=master_dims,
+            hidden_nonlinearity=nonlinearity or torch.relu,
+            w_init=torch.nn.init.xavier_uniform_,
+            input_dim=args.dim_option * 2,
+            output_dim=args.dim_option,
+            layer_normalization=False, #args.phi_layer_norm,
+        )
+        pred_net = module_cls(**module_kwargs)
 
-    #     module_cls, module_kwargs = get_gaussian_module_construction(
-    #         args,
-    #         hidden_sizes=master_dims,
-    #         hidden_nonlinearity=nonlinearity or torch.relu,
-    #         w_init=torch.nn.init.xavier_uniform_,
-    #         input_dim=args.dim_option,
-    #         output_dim=output_dim,
-    #         layer_normalization=False, #args.phi_layer_norm,
-    #         spectral_normalization=True,
-    #     )
-    #     z_encoder = module_cls(**module_kwargs)
-    # else:
-        # pred_net = None
+        module_cls, module_kwargs = get_gaussian_module_construction(
+            args,
+            hidden_sizes=master_dims,
+            hidden_nonlinearity=nonlinearity or torch.relu,
+            w_init=torch.nn.init.xavier_uniform_,
+            input_dim=args.dim_option,
+            output_dim=output_dim,
+            layer_normalization=False, #args.phi_layer_norm,
+            spectral_normalization=True,
+        )
+        z_encoder = module_cls(**module_kwargs)
+    else:
+        pred_net = None
     skill_dynamics = None
     pred_net = None
 
@@ -711,8 +711,8 @@ def run(ctxt=None):
             hidden_nonlinearity=nonlinearity or torch.relu,
             output_dim=args.dim_option,
         )
-        # if args.encoder:
-            # qf1 = with_encoder(qf1)
+        if args.encoder:
+            qf1 = with_encoder(qf1)
 
         qf2 = ContinuousMLPQFunctionEx(
             obs_dim=policy_q_input_dim,
@@ -721,8 +721,8 @@ def run(ctxt=None):
             hidden_nonlinearity=nonlinearity or torch.relu,
             output_dim=args.dim_option,
         )
-        # if args.encoder:
-            # qf2 = with_encoder(qf2)
+        if args.encoder:
+            qf2 = with_encoder(qf2)
 
         log_alpha = ParameterModule(torch.Tensor([np.log(args.alpha)]))
         optimizers.update({
@@ -888,89 +888,89 @@ def run(ctxt=None):
             **algo_kwargs,
             **skill_common_args,
         )
-    # elif args.algo == 'cic':
-    #     skill_common_args.update(
-    #         inner=args.inner,
-    #         num_alt_samples=args.num_alt_samples,
-    #         split_group=args.split_group,
-    #         dual_reg=args.dual_reg,
-    #         dual_slack=args.dual_slack,
-    #         dual_dist=args.dual_dist,
-    #     )
+    elif args.algo == 'cic':
+        skill_common_args.update(
+            inner=args.inner,
+            num_alt_samples=args.num_alt_samples,
+            split_group=args.split_group,
+            dual_reg=args.dual_reg,
+            dual_slack=args.dual_slack,
+            dual_dist=args.dual_dist,
+        )
 
-    #     algo = CIC(
-    #         **algo_kwargs,
-    #         **skill_common_args,
+        algo = CIC(
+            **algo_kwargs,
+            **skill_common_args,
 
-    #         pred_net=pred_net,
-    #         z_encoder=z_encoder,
-    #         cic_temp=args.cic_temp,
-    #         cic_alpha=args.cic_alpha,
-    #         knn_k=args.apt_knn_k,
-    #         rms=args.apt_rms,
-    #         alive_reward=args.alive_reward,
+            pred_net=pred_net,
+            z_encoder=z_encoder,
+            cic_temp=args.cic_temp,
+            cic_alpha=args.cic_alpha,
+            knn_k=args.apt_knn_k,
+            rms=args.apt_rms,
+            alive_reward=args.alive_reward,
 
-    #         dual_dist_scaling=args.dual_dist_scaling,
-    #         const_scaler=args.const_scaler,
-    #         wdm=args.wdm,
-    #         wdm_cpc=args.wdm_cpc,
-    #         wdm_idz=args.wdm_idz,
-    #         wdm_ids=args.wdm_ids,
-    #         wdm_diff=args.wdm_diff,
-    #         aug=args.aug,
-    #         joint_train=args.joint_train,
-    #     )
-    # elif args.algo == 'sac':
-    #     algo_kwargs.update(
-    #         use_discrete_sac=args.use_discrete_sac,
-    #     )
+            dual_dist_scaling=args.dual_dist_scaling,
+            const_scaler=args.const_scaler,
+            wdm=args.wdm,
+            wdm_cpc=args.wdm_cpc,
+            wdm_idz=args.wdm_idz,
+            wdm_ids=args.wdm_ids,
+            wdm_diff=args.wdm_diff,
+            aug=args.aug,
+            joint_train=args.joint_train,
+        )
+    elif args.algo == 'sac':
+        algo_kwargs.update(
+            use_discrete_sac=args.use_discrete_sac,
+        )
 
-    #     algo = SAC(
-    #         **algo_kwargs,
-    #         **skill_common_args
-    #     )
-    # elif args.algo == 'dads': # TODO: check args here if we do run it ourselves
-    #     algo_kwargs.update(
-    #         metra_mlp_rep=args.metra_mlp_rep,
-    #         f_encoder=f_encoder,
-    #         self_normalizing=args.self_normalizing,
-    #         log_sum_exp=args.log_sum_exp,
-    #         add_log_sum_exp_to_rewards=args.add_log_sum_exp_to_rewards,
-    #         fixed_lam=args.fixed_lam,
-    #         add_penalty_to_rewards=args.add_penalty_to_rewards,
-    #         no_diff_in_rep=args.no_diff_in_rep,
-    #         use_discrete_sac=args.use_discrete_sac,
-    #         turn_off_dones=args.turn_off_dones,
-    #         eval_goal_metrics=args.eval_goal_metrics,
-    #         goal_range=args.goal_range,
-    #         frame_stack=args.frame_stack,
-    #         sample_new_z=args.sample_new_z,
-    #         num_negative_z=args.num_negative_z,
-    #         infonce_lam=args.infonce_lam,
-    #         diayn_include_baseline=args.diayn_include_baseline,
-    #         uniform_z=args.uniform_z,
-    #     )
+        algo = SAC(
+            **algo_kwargs,
+            **skill_common_args
+        )
+    elif args.algo == 'dads': # TODO: check args here if we do run it ourselves
+        algo_kwargs.update(
+            metra_mlp_rep=args.metra_mlp_rep,
+            f_encoder=f_encoder,
+            self_normalizing=args.self_normalizing,
+            log_sum_exp=args.log_sum_exp,
+            add_log_sum_exp_to_rewards=args.add_log_sum_exp_to_rewards,
+            fixed_lam=args.fixed_lam,
+            add_penalty_to_rewards=args.add_penalty_to_rewards,
+            no_diff_in_rep=args.no_diff_in_rep,
+            use_discrete_sac=args.use_discrete_sac,
+            turn_off_dones=args.turn_off_dones,
+            eval_goal_metrics=args.eval_goal_metrics,
+            goal_range=args.goal_range,
+            frame_stack=args.frame_stack,
+            sample_new_z=args.sample_new_z,
+            num_negative_z=args.num_negative_z,
+            infonce_lam=args.infonce_lam,
+            diayn_include_baseline=args.diayn_include_baseline,
+            uniform_z=args.uniform_z,
+        )
 
-    #     skill_common_args.update(
-    #         inner=args.inner,
-    #         num_alt_samples=args.num_alt_samples,
-    #         split_group=args.split_group,
-    #         dual_reg=args.dual_reg,
-    #         dual_slack=args.dual_slack,
-    #         dual_dist=args.dual_dist,
-    #     )
+        skill_common_args.update(
+            inner=args.inner,
+            num_alt_samples=args.num_alt_samples,
+            split_group=args.split_group,
+            dual_reg=args.dual_reg,
+            dual_slack=args.dual_slack,
+            dual_dist=args.dual_dist,
+        )
 
-    #     algo = DADS(
-    #         **algo_kwargs,
-    #         **skill_common_args,
-    #     )
-    # elif args.algo == 'ppo':
-        # algo = PPO(
-        #     **algo_kwargs,
-        #     vf=vf,
-        #     gae_lambda=0.95,
-        #     ppo_clip=0.2,
-        # )
+        algo = DADS(
+            **algo_kwargs,
+            **skill_common_args,
+        )
+    elif args.algo == 'ppo':
+        algo = PPO(
+            **algo_kwargs,
+            vf=vf,
+            gae_lambda=0.95,
+            ppo_clip=0.2,
+        )
     else:
         raise NotImplementedError
 
